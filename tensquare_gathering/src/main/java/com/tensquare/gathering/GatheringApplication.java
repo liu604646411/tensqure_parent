8ac07b9bc6dcc46196736e6d35f712e4397a6acc
mr_liu <604646411@qq.com>
2020-11-01 22:07:40 +0800

    更新启动类名称

--- tensquare_gathering/src/main/java/com/tensquare/gathering/GatheringApplication.java
+++ tensquare_gathering/src/main/java/com/tensquare/gathering/GatheringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
 @SpringBootApplication
-public class Application {
+public class GatheringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(GatheringApplication.class, args);
 	}
 
 	@Bean
